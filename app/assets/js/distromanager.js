522e278a49cca7802fec6f2be806b4a9419da5a5
Nefers12 <steoxys42@gmail.com>
2024-02-11 11:41:42 +0100

    distro

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://158.178.197.251:8080/shioh.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
